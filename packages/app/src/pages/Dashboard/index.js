5eb3039a1a431c4f020dc9b420d7ffb3fed2ffae
Maurício Pires Cardoso <mauricio@ema.net.br>
2019-11-19 09:23:12 -0300

    GoBarber Web - Estilizando do Dashboard

--- packages/app/src/pages/Dashboard/index.js
+++ packages/app/src/pages/Dashboard/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import api from '~/service/api';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>19 de Novembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Mauricio Cardoso</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Mauricio Cardoso</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Mauricio Cardoso</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
